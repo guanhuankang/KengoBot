4568963cc0b09c8cec04763c99e3a53037b1b1c8
guanhuankang <2659814334@qq.com>
2022-10-15 23:03:03 +0800

    reply image

--- kengobot/main.py
+++ kengobot/main.py
@@ -30,11 +30,15 @@ def wx():
     if data.isTextMsg():
         if data.content=="openid":
             return response.textMsg(openid)
+        if data.content=="ip":
+            return response.textMsg("45.32.16.38")
+        if data.content=="piclink":
+            return response.textMsg("http://45.32.16.38/static/{}.jpg".format(openid))
         else:
             return response.textMsg(text=data.content)
     if data.isImageMsg():
         img = data.content
-        filepath = os.path.join(HOME_DIR, "static/{}.png".format(openid))
+        filepath = os.path.join(HOME_DIR, "static/{}.jpg".format(openid))
         logging.info("save to local filepath:{}".format(filepath))
         img.save(filepath)
         media_id = api.uploadImage(filepath)
